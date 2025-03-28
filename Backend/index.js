c7c7c104c8f6b92c9fae9c6b8ee83663f3a65467
Hepsibaalkolu <hepsiba215@gmail.com>
2025-03-28 16:15:57 +0530

    redeploy

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ connectToMongo();
 
 const express = require('express')
 const app = express()
-const port = 3001
+
 
 const cors = require('cors')
 const router = require('./Routes/router')
@@ -12,8 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
